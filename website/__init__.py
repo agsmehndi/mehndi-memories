5380c9f80bdb421318327a839cbc69f44d16a19a
Rhythm Mutha <agsmehndi@gmail.com>
2025-05-20 16:01:34 +0530

    Ghibli Removed + Try to fix mirror error

--- website/__init__.py
+++ website/__init__.py
@@ -7,13 +7,11 @@ def create_app():
 
     from .views import views
     from .polaroid import polaroid
-    from .ghibli import ghibli
     from .vfb import vfb
     from .film import film
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(polaroid, url_prefix='/')
-    app.register_blueprint(ghibli, url_prefix='/')
     app.register_blueprint(vfb, url_prefix='/')
     app.register_blueprint(film, url_prefix='/')
 
--- website/ghibli.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask import Blueprint
-
-ghibli = Blueprint('ghibli', __name__)
-
-@ghibli.route('/ghibli/en')
-def Ghibli():
-    return "<p>Ghibli</p>"
-
