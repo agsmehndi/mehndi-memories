dbeb22f8e6625ac257f6e0af84e86cc75564f1aa
Rhythm Mutha <agsmehndi@gmail.com>
2025-05-21 12:07:33 +0530

    Rerun

--- website/__init__.py
+++ website/__init__.py
@@ -8,11 +8,9 @@ def create_app():
     from .views import views
     from .polaroid import polaroid
     from .vfb import vfb
-    from .film import film
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(polaroid, url_prefix='/')
     app.register_blueprint(vfb, url_prefix='/')
-    app.register_blueprint(film, url_prefix='/')
 
     return app
